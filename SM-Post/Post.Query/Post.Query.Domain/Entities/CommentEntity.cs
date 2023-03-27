6addfc8d9c27da3a04f7a577ef69b5893451e61e
rahib533 <rahib.jafar.98@gmail.com>
2023-03-27 15:58:24 +0400

    adding of repositories and entities

---
+++ SM-Post/Post.Query/Post.Query.Domain/Entities/CommentEntity.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Post.Query.Domain.Entities
+{
+    [Table("Comment")]
+    public class CommentEntity
+    {
+        public Guid CommentId { get; set; }
+        public string Username { get; set; }
+        public DateTime CommentDate { get; set; }
+        public string Comment { get; set; }
+        public bool Edited { get; set; }
+        public Guid PostId { get; set; }
+        [JsonIgnore]
+        public virtual PostEntity Post { get; set; }
+    }
+}
